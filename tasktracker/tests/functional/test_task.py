d07d26312f836c2260df1bf8ce09097a6beda211
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2006-10-04 21:07:32 +0000

    Hide reorder button if fewer than two tasks in list.  Hide task columns if no tasks in list.  Prohibit adding emptystring as a status.
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@1240 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/tests/functional/test_task.py
+++ tasktracker/tests/functional/test_task.py
@@ -17,6 +17,8 @@ class TestTaskController(TestController):
         assert task.status == 'not done'
         res2 = app.post('/task/change_status/%s' % task.id, params={'status':'done'})
         assert task.status == "done"
+        res2 = app.post('/task/change_status/%s' % task.id, params={'status':'__no_such_status__'})
+        assert task.status == "done"
      
     def test_show_create(self):
         task_listID = TaskList.select()[0].id
