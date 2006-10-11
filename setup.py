f7dbfc7e4417b14183ab28af1dc4ebb0f7822435
David Turner <novalis@novalis.org>
2006-10-11 17:12:19 +0000

    Added zopepagetemplates from dependency_links.
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@1326 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     include_package_data=True,
     test_suite = 'nose.collector',
     package_data={'tasktracker': ['i18n/*/LC_MESSAGES/*.mo']},
+    dependency_links = ["http://pythonpaste.org/package_index.html"],
     entry_points="""
     [paste.app_factory]
     main=tasktracker:make_app
