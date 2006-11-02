cb3e17ddc8145c1692def4164e6773223c73f1a5
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2006-11-02 16:28:08 +0000

    changed import test: put expected tasks in a dict, and made the worddoc.doc test dumber (reflecting what the parser tries to do)
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@1526 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/tests/test_import.py
+++ tasktracker/tests/test_import.py
@@ -19,32 +19,37 @@
 # USA
 
 from tasktracker.lib.taskparser import TaskParser
-from tasktracker.tests import *
+#from tasktracker.tests import *
 
 import datetime
 import os
 
-class TestImport(TestController):
+class TestImport:
 
     def test_parse(self):
-        expected_tasks = [dict(deadline = datetime.datetime(2006, 9, 21), 
-                           title = 'write press release for fleem morx'), 
-                          dict(deadline = datetime.datetime(2006, 9, 13),
-                               title = 'blah blah blah'),
-                          dict(deadline = datetime.datetime(2006, 9, 4),
-                               title = 'morx')
-                          ]
-        datadir = "tasktracker/tests/data"
+        expected_tasks = {'blah blah blah':
+                              dict(deadline = datetime.datetime(2006, 9, 13),
+                                   title = 'blah blah blah'),
+                          'morx':
+                              dict(deadline = datetime.datetime(2006, 9, 4),
+                                   title = 'morx'),
+                          'write press release for fleem morx (bob)':
+                              dict(deadline = datetime.datetime(2006, 9, 21), 
+                                   title = 'write press release for fleem morx (bob)')
+                          }
+        datadir = "data"
         files = os.listdir(datadir)
         for file in files:
             path = os.path.join(datadir, file)
             if not os.path.isfile(path):
                 continue
-            tasks = TaskParser.parse(path)
 
+            tasks = TaskParser.parse(path)
             assert len(tasks) == len(expected_tasks)
-            for i in range(len(tasks)):
-                assert tasks[i]['title'] == expected_tasks[i]['title']
-                assert tasks[i]['deadline'].month == expected_tasks[i]['deadline'].month
-                assert tasks[i]['deadline'].day == expected_tasks[i]['deadline'].day
-                assert tasks[i]['deadline'].year == expected_tasks[i]['deadline'].year or expected_tasks[i]['deadline'].year == 2006
+            for task in tasks:
+                assert expected_tasks.has_key(task['title'])
+                ex_task = expected_tasks[task['title']]
+                assert task['title'] == ex_task['title']
+                assert task['deadline'].month == ex_task['deadline'].month
+                assert task['deadline'].day == ex_task['deadline'].day
+                assert task['deadline'].year == ex_task['deadline'].year
