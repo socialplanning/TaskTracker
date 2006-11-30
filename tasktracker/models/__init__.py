23585b0eaaf3df4811013c97bbc81d9a34597a43
David Turner <novalis@novalis.org>
2006-11-30 22:05:26 +0000

    removed circular imports.  This is the first case where Python seems to me as actively hostile to programmers as other languages
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@1910 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/models/__init__.py
+++ tasktracker/models/__init__.py
@@ -27,6 +27,7 @@ from sqlobject.inheritance import InheritableSQLObject
 from sqlobject.sqlbuilder import *
 from pylons.database import PackageHub
 from pylons import c
+import tasktracker.lib.helpers as h
 
 hub = PackageHub("tasktracker", pool_connections=False)
 __connection__ = hub
@@ -296,8 +297,7 @@ class Task(SQLObject):
                 task.sort_index += 1
 
     def liveChildren(self):
-        from tasktracker.lib.helpers import has_permission
-        return [c for c in self.children if c.live and has_permission('task', 'show', id=c.id)]
+        return [c for c in self.children if c.live and h.has_permission('task', 'show', id=c.id)]
 
     def isOwnedBy(self, username):
         return self.owner == username            
@@ -377,20 +377,16 @@ class TaskList(SQLObject):
         return Watcher.selectBy(username=username, task_list=self.id).count() > 0
 
     def topLevelTasks(self):
-        from tasktracker.lib.helpers import has_permission
-        return [c for c in Task.selectBy(parentID=0, live=True, task_listID=self.id) if has_permission('task', 'show', id=c.id)]
+        return [c for c in Task.selectBy(parentID=0, live=True, task_listID=self.id) if h.has_permission('task', 'show', id=c.id)]
 
     def uncompletedTasks(self):
-        from tasktracker.lib.helpers import has_permission
-        return [c for c in Task.select(AND(Task.q.status != 'done', Task.q.task_listID == self.id, Task.q.live == True)) if has_permission('task', 'show', id=c.id)]
+        return [c for c in Task.select(AND(Task.q.status != 'done', Task.q.task_listID == self.id, Task.q.live == True)) if h.has_permission('task', 'show', id=c.id)]
     
     def completedTasks(self):
-        from tasktracker.lib.helpers import has_permission
-        return [c for c in Task.select(AND(Task.q.status == 'done', Task.q.task_listID == self.id, Task.q.live == True)) if has_permission('task', 'show', id=c.id)]
+        return [c for c in Task.select(AND(Task.q.status == 'done', Task.q.task_listID == self.id, Task.q.live == True)) if h.has_permission('task', 'show', id=c.id)]
 
     def visibleTasks(self):
-        from tasktracker.lib.helpers import has_permission
-        return [c for c in Task.select(AND(Task.q.task_listID == self.id, Task.q.live == True)) if has_permission('task', 'show', id=c.id)]
+        return [c for c in Task.select(AND(Task.q.task_listID == self.id, Task.q.live == True)) if h.has_permission('task', 'show', id=c.id)]
 
     def set(self, **kwargs):
 
