fa2c68f9cd5aeac93567611128d892227cdc4486
David Turner <novalis@novalis.org>
2006-12-01 22:04:24 +0000

    added initial stab at date prettifier (needs work, but I need to be at home)
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@1939 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ tasktracker/public/javascripts/pretty-date.js
@@ -0,0 +1,58 @@
+function utc(d) {
+    return Date.UTC(d.getFullYear(), d.getMonth(), d.getDate());
+}
+
+function date_diff(date1, date2) {
+    var date1_UTC = utc(date1);
+    var date2_UTC = utc(date2);
+    console.log(date1, date2, date1_UTC / 86400000, date2_UTC / 86400000);
+    return (date1_UTC - date2_UTC) / 86400000;
+}
+
+function dayOfWeek(day) {
+    return "SunMonTueWedThuFriSatSun".substr(day * 3, 3);
+}
+
+
+function pretty_date_engine(now, date) {
+    var diff = date_diff(date, now);
+    if (diff == -1) {
+	return "Yesterday";
+    }
+    if (diff == 0) {
+	return "Today";
+    }
+    if (diff == 1) {
+	return "Tomorrow";
+    }
+    if (diff < 7) {
+	return dayOfWeek(date.getDay());
+    }
+}
+
+function pretty_date(date) {
+    var now = new Date();
+    return pretty_date_engine(now, date);
+}
+
+
+function test_pretty_date() {
+    var now = new Date(2006, 0, 1);
+    var dates = {
+	'Today' : new Date(2006, 0, 1),
+	'Tomorrow' : new Date(2006, 0, 2),
+	'Yesterday' : new Date(2005, 11, 31),
+	'Tue' : new Date(2006, 0, 3),
+	'Sat' : new Date(2006, 0, 7),
+	'January 8' : new Date(2006, 0, 8),
+	'January 8, 2007' : new Date(2007, 0, 8)
+    };
+
+    for (date in dates) {
+	var pd = pretty_date_engine(now, dates[date]);
+	if (pd != date) {
+	    alert("failure mapping " + dates[date] + " to " + date + ", got " + pd);
+	    break;
+	}
+    }
+}
