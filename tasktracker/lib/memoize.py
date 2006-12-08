95d69d4a93d6a1df1e03655a819459ba8c5ccf61
David Turner <novalis@novalis.org>
2006-12-08 14:39:53 +0000

    memoize -- I submitted this to Pylons, but it is not in yet
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@2024 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ tasktracker/lib/memoize.py
@@ -0,0 +1,17 @@
+from pylons.decorator import decorator
+from pylons import c
+
+def _make_key(f, args, kw):
+    return tuple([f, tuple(args), tuple(sorted(kw.items()))])
+
+def memoize(f, *args, **kw):
+    if not c.memos:
+        c.memos = {}
+
+    key = _make_key(f, args, kw)
+    if not c.memos.has_key(key):
+        c.memos[key] = f(*args, **kw)
+
+    return c.memos[key]
+
+memoize=decorator(memoize)
