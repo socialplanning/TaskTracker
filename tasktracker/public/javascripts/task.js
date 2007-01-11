socialplanning/TaskTracker

commit ede4736efe67d76175cadead50fc72bb3fcfb13c

now forms are more secure
git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@2309 40f73b84-6177-4a8e-8c61-7eee4b1974ed

tasktracker/public/javascripts/task.js
@@ -505,7 +505,8 @@ function failedAddingTask(req) {
 function changeField(task_id, fieldname) {
     var field = $(fieldname + '_' + task_id);
     field.disabled = true;
-    var url = '/task/change_field/' + task_id + '?field=' + fieldname;
+    var authenticator = $('authenticator').value;
+    var url = '/task/change_field/' + task_id + '?field=' + fieldname + '&authenticator=' + authenticator;
     var value = (field.type == 'checkbox') ? field.checked : field.value;
     var req = new Ajax.Request(url, {asynchronous:true, evalScripts:true, method:'post', parameters:fieldname + '=' + value,
 				     onSuccess:doneChangingField.bind([task_id, fieldname]), onFailure:failedChangingField.bind([task_id, fieldname])});

tasktracker/templates/task/show_create.pt
@@ -1,6 +1,6 @@
 <metal:macro define-macro="show_create">
 
-<div tal:replace="structure python:h.form_remote_tag(url=h.url_for(controller='task', action='create'),
+<div tal:replace="structure python:h.secure_form_remote_tag(url=h.url_for(controller='task', action='create'),
       			     success='return doneAddingTask(request);', failure='failedAddingTask', html=dict(id='add_task_form'))" />
 
 Add task or sub-task:

tasktracker/templates/tasklist/show_create.pt
@@ -5,7 +5,7 @@ tasks by area, or by sub-project.  For example, there might be a task
 list for tasks around the office, and another task list for organizing
 opposition to a proposed law.
 
-<span tal:replace="structure python: h.form(h.url(action='create'), method='post')" />
+<span tal:replace="structure python: h.secure_form(h.url(action='create'), method='post')" />
 
 <metal:do use-macro="here/_form/macros/form" />
 

tasktracker/controllers/task.py
@@ -60,6 +60,7 @@ class TaskController(BaseController):
                 clean[param] = params[param]
         return clean
     
+    @authenticate
     @validate(schema=EditTaskForm(), form='show_update')
     @attrs(action=_field_permission, watchdog=TaskUpdateWatchdog)
     @catches_errors
@@ -143,6 +144,7 @@ class TaskController(BaseController):
             c.parentID = 0
         return render_response('zpt', 'task.show_create')
 
+    @authenticate
     @attrs(action='create', watchdog=TaskCreateWatchdog)
     @validate(schema=EditTaskForm(), form='show_create')
     def create(self, *args, **kwargs):
@@ -200,6 +202,7 @@ class TaskController(BaseController):
         c.owner = c.oldtask.owner
         return render_response('zpt', 'task.show_update')
 
+    @authenticate
     @attrs(action='update', watchdog=TaskUpdateWatchdog)
     @validate(schema=EditTaskForm(), form='show_update')
     def update(self, id):
@@ -235,6 +238,7 @@ class TaskController(BaseController):
 
         return Response.redirect_to(action='show', controller='tasklist', id=c.task.task_listID)
 
+    @authenticate
     @attrs(action='private')
     def update_private(self, id):
         c.task = self._getTask(int(id))
@@ -260,6 +264,7 @@ class TaskController(BaseController):
         c.flat = True
         return render_response('zpt', 'task.show')
 
+    @authenticate
     @attrs(action='update')
     @catches_errors
     def destroy(self, id, *args, **kwargs):
@@ -267,6 +272,7 @@ class TaskController(BaseController):
         c.task.live = False
         return Response.redirect_to(action='show', controller='tasklist', id=c.task.task_listID)
 
+    @authenticate
     @attrs(action='create')
     def create_tasks(self):
         tasks = request.params['tasks']

tasktracker/controllers/tasklist.py
@@ -148,6 +148,7 @@ class TasklistController(BaseController):
             if manager and not manager in administrators:
                 TaskListRole(task_listID=tasklist.id, username=manager,roleID=list_owner)
 
+    @authenticate
     @attrs(action='create')
     @validate(schema=CreateListForm(), form='show_create')  
     def create(self):
@@ -180,6 +181,7 @@ class TasklistController(BaseController):
         else:
             return filled_render('tasklist.show_preferences', c.tasklist, p)
 
+    @authenticate
     @validate(schema=CreateListForm(), form='show_update')  
     @attrs(action='update')
     @catches_errors
@@ -202,6 +204,7 @@ class TasklistController(BaseController):
         except LookupError:
             raise NoSuchIdError("No such tasklist ID: %s" % id)
 
+    @authenticate
     @attrs(action='create')
     @catches_errors
     def destroy(self, id, *args, **kwargs):

tasktracker/lib/base.py
@@ -76,6 +76,8 @@ def catches_errors(f):
     new_f.func_name = f.func_name
     return new_f
 
+from tasktracker.lib.secure_forms import authenticate
+
 def attrs(**kwds):
     def decorate(f):
         for k in kwds:

tasktracker/lib/helpers.py
@@ -36,6 +36,7 @@ import imp, os
 
 from formencode import htmlfill
 from tasktracker.lib.base import render_response
+from tasktracker.lib.secure_forms import *
 
 from tasktracker.lib.pretty_date import prettyDate
 from random import random
@@ -418,7 +419,7 @@ def render_action(action):
         if not fields:
             return ''
         comment = "%s updated %s by %s" % (", ".join (fields), prettyDate(action.updated), action.updated_by)
-    return '<li>%s</li>' % comment
+    return '<li>%s<hr/></li>' % comment
 
 
 def field_last_updated(task, field):

tasktracker/lib/secure_forms.py
@@ -1,0 +1,59 @@
+import random
+from pylons import g, session
+from webhelpers import form, hidden_field, link_to_remote, link_to, form_remote_tag
+from pylons.decorator import decorator
+from pylons import request, Response
+
+def session_key():
+    if not session.has_key('secure_id'):
+        session['secure_id'] = str(random.getrandbits(128))
+    session.save()
+    return session['secure_id']
+
+def secure_form(url, **args):
+    id = session_key()
+    f = form(url, **args)
+    return f + hidden_field('authenticator', id)
+
+def secure_form_remote_tag(**args):
+    id = session_key()
+    f = form_remote_tag(**args)
+    return f + hidden_field('authenticator', id)
+
+
+def authenticate_form(params):
+    return params['authenticator'] == session.get('secure_id', None)
+    
+
+def authenticate(func, *args, **kw):
+    """Action decorator that, with secure_form, prevents
+    certain cross-site scripting attacks.
+    """
+
+    if authenticate_form(request.params):
+        del request.params['authenticator']
+        return func(*args, **kw)
+    else:
+        return Response("Bad authenticator (cat got your session cookie?)", code=403)
+    
+authenticate = decorator(authenticate)
+
+def secure_link_to_remote(text, params, **args):
+    if '?' in params['url']:
+        separator = '&'
+    else:
+        separator = '?'
+
+    params['url'] += '%sauthenticator=%s' % (separator, session_key())
+    return link_to_remote(text, params, **args)
+    
+
+def secure_link_to(text, url, **args):
+    if '?' in url:
+        separator = '&'
+    else:
+        separator = '?'
+
+    url += '%sauthenticator=%s' % (separator, session_key())
+    return link_to(text, url, **args)
+    

commit 51e3a4c1ac3553b34d50532ab4207bf99598b4b0

fixed add_task javascript broken by r2309 (authentication moved around order of inputs in creation form)
git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@2348 40f73b84-6177-4a8e-8c61-7eee4b1974ed

tasktracker/public/javascripts/task.js
@@ -434,15 +434,16 @@ function sortAndFilter() {
 function restoreAddTask() { 
     $('add_task_anchor').appendChild($('movable_add_task'));
     showTaskCreate();
-    $('add_task_form').getInputs()[1].setAttribute("value", 0);
-    $('add_task_form').getInputs()[2].setAttribute("value", 0);
+    $('add_task_form_parentID').setAttribute("value", 0);
+    $('add_task_form_siblingID').setAttribute("value", 0);
     return false;
 }
 
 function doneAddingTask(req) {
-    var forminputs = $('add_task_form').getInputs();
-    var parentID = parseInt(forminputs[1].getAttribute("value"));
-    var siblingID = parseInt(forminputs[2].getAttribute("value"));
+    var parentID = parseInt($('add_task_form_parentID').getAttribute("value"));
+    var siblingID = parseInt($('add_task_form_siblingID').getAttribute("value"));
+    console.log("parentID: " + parentID);
+    console.log("siblingID: " + siblingID);
     var new_fragment = evalHTML(req.responseText);
     var new_item = new_fragment.firstChild; 
 
@@ -832,8 +833,8 @@ function doDrop(child, drop_target, a) {
     if (!child.id.match("draggable")) {  // TODO be more specific
 	if (drop_target.id.match(/^title_/)) {   // drop under a parent node
 	    id = parseInt(drop_target.id.replace(/^title_/, ''));
-	    $('add_task_form').getInputs()[1].setAttribute("value", id);
-	    $('add_task_form').getInputs()[2].setAttribute("value", 0);
+	    $('add_task_form_parentID').setAttribute("value", id);
+	    $('add_task_form_siblingID').setAttribute("value", 0);
 	    var new_parent = $('task_' + id);
 	    var tr = document.createElement("TR");
 	    tr.className = "taskrow";
@@ -842,8 +843,8 @@ function doDrop(child, drop_target, a) {
 	    // todo indentation
 	} else {   // drop after a sibling node
 	    id = parseInt(drop_target.id.replace(/^handle_/, ''));
-	    $('add_task_form').getInputs()[1].setAttribute("value", 0);
-	    $('add_task_form').getInputs()[2].setAttribute("value", id);
+	    $('add_task_form_parentID').setAttribute("value", 0);
+	    $('add_task_form_siblingID').setAttribute("value", id);
 	    var new_sibling = $('task_' + id);
 	    var ul = new_sibling.parentNode;
 	    var li = document.createElement("li");
@@ -1042,3 +1043,4 @@ function setPermalink(newkey, newval) {
 
 addLoadEvent(function () { with_items ("unfolded", add_unfold, document.childNodes[0]); });
 addLoadEvent(sortAndFilter);
+addLoadEvent(restoreAddTask);

tasktracker/public/stylesheets/list.css
@@ -1,6 +1,6 @@
 body {
    margin-left: 5%;
-   margin-right: 40%;
+   margin-right: 5%;
    margin-top: 5%;
 }
 

tasktracker/templates/task/list.pt
@@ -34,7 +34,8 @@
 <div class="draggable" id="movable_add_task" parentID="0" siblingID="0" style="padding:0px;  margin:0px; line-height: 1em;">
 <metal:do use-macro="here/_hideable_show_create/macros/show">
 <div metal:fill-slot="subtask_link">
-[ + ]<a id="add_a_task" tal:attributes="href python:h.url_for(action='show_create', controller='task', task_listID=c.tasklist.id)" onclick="return false;">add a task</a>
+[ + ]<a id="add_a_task" tal:attributes="href python:h.url_for(action='show_create', controller='task', task_listID=c.tasklist.id)"
+    	onclick="return false;">add a task</a>
 </div>
 </metal:do>
 </div></div>

tasktracker/templates/task/show_create.pt
@@ -7,8 +7,8 @@ Add task or sub-task:
 
 <br/><br/>
 <input type="hidden" name="task_listID" tal:attributes="value c/task_listID" />
-<input type="hidden" name="parentID" tal:attributes="value c/parentID" />
-<input type="hidden" name="siblingID" value="0" />
+<input type="hidden" id="add_task_form_parentID" name="parentID" tal:attributes="value c/parentID" />
+<input type="hidden" id="add_task_form_siblingID" name="siblingID" value="0" />
 
 <div id="hideable_title_label" style="display:none;">title</div>
 <div tal:replace="structure python: h.text_field_r('oldtask', 'title', size=80, tabindex=1)" />