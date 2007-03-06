02c8e43adcd57d8cc857884f05f98b7514f7efcd
David Turner <novalis@novalis.org>
2007-03-06 20:50:56 +0000

    slightly better error reporting for missing projects
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@3362 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/lib/project_members.py
+++ tasktracker/lib/project_members.py
@@ -5,6 +5,8 @@ def get_users_for_project(project, server):
 
     h = httplib2.Http()
     resp, content = h.request("%s/projects/%s/members.xml" % (server, project), "GET")
+    if resp['status'] != 200:
+        raise ValueError("Error retrieving project %s: status %s" % (project, resp['status']))
     tree = ET.fromstring(content)
     members = []
     for member in tree:
@@ -20,6 +22,8 @@ def get_info_for_project(project, server):
 
     h = httplib2.Http()
     resp, content = h.request("%s/projects/%s/info.xml" % (server, project), "GET")
+    if resp['status'] != 200:
+        raise ValueError("Error retrieving project %s: status %s" % (project, resp['status']))
     tree = ET.fromstring(content)
     policy = tree[0]
     assert policy.tag == "policy"
