35465af18cb1e5c43ed9b8291e6d63af2c6a6881
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-03-26 15:01:39 +0000

    added license and made function not-absurd.
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@3773 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/lib/cache.py
+++ tasktracker/lib/cache.py
@@ -1,24 +1,32 @@
-#caches = {}
+
+# Copyright (C) 2006 The Open Planning Project
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the 
+# Free Software Foundation, Inc., 
+# 51 Franklin Street, Fifth Floor, 
+# Boston, MA  02110-1301
+# USA
 
 def get_cached(environ, cache_name, key, default_func,
-               expiretime = 60, default_args = None, default_kwargs = None):
-#    global caches
+               expiretime = 60, *default_args, **default_kwargs):
     beaker = environ['beaker.cache']
-#    if not caches.has_key(cache_name):
-#        caches[cache_name] = beaker.get_cache(cache_name)  # why? -egj
-    
-#    cache = caches[cache_name]
     cache = beaker.get_cache(cache_name)
 
     value = None
     if key in cache:
         value = cache.get_value(key, expiretime=expiretime)
     if not value:
-# @@ why? - egj
-        if default_args is None:
-            default_args = []
-        if default_kwargs is None:
-            default_kwargs = {}
         value = default_func(*default_args, **default_kwargs)
         cache.set_value(key, value)
     return value
