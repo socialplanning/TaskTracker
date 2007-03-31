527c33dc015489d547e6d1ccb66e413c606dbf22
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-03-31 15:57:21 +0000

    cleanup (moved project_members stuff into cookieauth where it belongs)
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@3934 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/lib/cookieauth.py
+++ tasktracker/lib/cookieauth.py
@@ -49,16 +49,46 @@ def get_secret():
     if not secret:
         secret_file_name = os.environ.get('TOPP_SECRET_FILENAME')
         assert secret_file_name
-
         f = open(secret_file_name)
         secret = f.readline().strip()
         f.close()
     return secret
 
-
-from project_members import *
-
 from tasktracker.lib import usermapper
+
+import httplib2
+import elementtree.ElementTree as ET
+
+from topp.utils import memorycache
+
+@memorycache.cache(120)
+def get_users_for_project(project, server):
+    h = httplib2.Http()
+    resp, content = h.request("%s/projects/%s/members.xml" % (server, project), "GET")
+    if resp['status'] != '200':
+        raise ValueError("Error retrieving project %s: status %s" % (project, resp['status']))
+    tree = ET.fromstring(content)
+    members = []
+    for member in tree:
+        m = {}
+        m['username'] = member.find('id').text
+        m['roles'] = []
+        for role in member.findall('role'):
+            m['roles'].append(role.text)
+        members.append(m)
+    return members
+
+@memorycache.cache(120)
+def get_info_for_project(project, server):
+    h = httplib2.Http()
+    resp, content = h.request("%s/projects/%s/info.xml" % (server, project), "GET")
+    if resp['status'] != '200':
+        raise ValueError("Error retrieving project %s: status %s" % (project, resp['status']))
+    tree = ET.fromstring(content)
+    policy = tree[0]
+    assert policy.tag == "policy"
+    info = dict(policy=policy.text)
+    return info
 
 class UserMapper(usermapper.UserMapper):
     def __init__(self, environ, project, server):
