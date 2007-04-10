36643eff108fca86e046059be29f870c7e75ef28
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-04-10 21:45:22 +0000

    updating some copyright dates
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@4137 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/websetup.py
+++ tasktracker/websetup.py
@@ -1,5 +1,5 @@
 
-# Copyright (C) 2006 The Open Planning Project
+# Copyright (C) 2006-2007 The Open Planning Project
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
