8f190a035fd540d24adf3346676544e33755e0e7
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-04-10 18:13:57 +0000

    lots of cleanup and more rigorous tests
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@4117 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/tests/functional/test_task.py
+++ tasktracker/tests/functional/test_task.py
@@ -23,91 +23,80 @@ from tasktracker.models import *
 import re
 
 class TestTaskController(TestController):
-    def test_index(self):
-        lists = list(TaskList.select())
 
-        app = self.getApp('admin')
-
-        res = app.get(url_for(controller='tasklist'))
-        for l in lists:
-            res.mustcontain(l.title)
-        res = res.click(lists[0].title)
-        res.mustcontain(lists[0].title)
-        task = lists[0].tasks[0]
-
+    def _get_authenticator(self, res):
         authenticator = None
         for form in res.forms.values():
             if form.get('authenticator', None):
                 authenticator = form['authenticator'].value
                 break
         assert authenticator
+        return authenticator
+
+    # @@ what the hell is this testing? -- egj
+    def test_index(self):
+        lists = list(TaskList.select())
+        app = self.getApp('admin')
+
+        res = app.get(url_for(controller='tasklist'))
+        for l in lists:
+            res.mustcontain(l.title)
+
+        res = res.click(lists[0].title)
+        res.mustcontain(lists[0].title)
+
+        task = lists[0].tasks[0]
+        authenticator = self._get_authenticator(res)
 
         assert task.status == 'not done'
-        res2 = app.post('/task/change_field/%s' % task.id, params={'field':'status', 'status':'true', 'authenticator':authenticator})
+        res2 = app.post('/task/change_field/%s' % task.id,
+                        params={'field':'status', 'status':'true', 'authenticator':authenticator})
         assert task.status == "done"
         try:
-            res2 = app.post('/task/change_field/%s' % task.id, params={'field':'status', 'status':'__no_such_status__', 'authenticator':authenticator})
+            res2 = app.post('/task/change_field/%s' % task.id,
+                            params={'field':'status', 'status':'__no_such_status__', 'authenticator':authenticator})
         except AssertionError:
             pass
         assert task.status == "done"
      
-    def test_show_create(self):
+    def test_create_task(self):
         task_listID = TaskList.select()[0].id
-
         app = self.getApp('admin')
         res = app.get(url_for(controller='tasklist', action='show', id=task_listID))
 
+        ### create a new task
         form = res.forms['add_task_form']
         form['title'] = 'The new task title'
         form['text'] = 'The new task body'
         res = form.submit()
 
-        # the response should consist of the new task's table row
-        print res.body
+        ### the response should consist of the new task's table row
         assert re.search('<tr[^>]*status\s*=\s*"not%20done"', res.body)
         res.mustcontain("The new task title")
         res.mustcontain("The new task body")
 
-        # the task should now show up on the tasklist view page
-        res = app.get(url_for(
-                controller='tasklist', action='show',
-                id=task_listID
-                ))
+        ### the task should now show up on the tasklist view page
+        res = app.get(url_for(controller='tasklist', action='show', id=task_listID))
         res.mustcontain("The new task title")
         res.mustcontain("The new task body")
-
-    def _getElementsByTagName(self, body, tagname):
-        """ This is a total hack and should not be used """
-        elements = []
-        start = -1
-        while 1:
-            start = body.find('<' + tagname, start + 1)
-            if start == -1:
-                break
-            end = body.find('>', start)
-            elements.append(body[start:end+1])
-
-        return elements
 
     def test_subtask(self):
         """ Test that subtasks show up on the page for the task they come from. """
         tl = self.create_tasklist('testing subtasks')
         top_level_task = Task(title='top level', task_listID = tl.id, text = '')
         sub_task = Task(title='second level', task_listID = tl.id, text = '', parentID = top_level_task.id)
-
         app = self.getApp('admin')
 
-        res = app.get(url_for(
-                controller='tasklist', action='show', id=tl.id))
+        ### both the top-level and subtask should show up on the tasklist
+        res = app.get(url_for(controller='tasklist', action='show', id=tl.id))
+        res.mustcontain('top level')
+        res.mustcontain('second level')
 
-        res.mustcontain ('top level')
-        res.mustcontain ('second level')
-
-        res = app.get(url_for(
-                controller='task', action='show', id=top_level_task.id))
-
-        res.mustcontain ('top level')
-        res.mustcontain ('second level')
+        ### and on the top-level task's page
+        res = app.get(url_for(controller='task', action='show', id=top_level_task.id))
+        res.mustcontain('top level')
+        res.mustcontain('second level')
+        res.mustcontain("1 </span> sub-task")
 
         sub_task.destroySelf()
         top_level_task.destroySelf()
@@ -115,7 +104,6 @@ class TestTaskController(TestController):
 
     def test_comments(self):
         tl = TaskList.select()[0]
-
         #set security such that any logged-in user can comment and show posts
         found = False
         for perm in tl.permissions:
@@ -123,25 +111,21 @@ class TestTaskController(TestController):
                 found = True
                 perm.min_level = Role.getLevel("Authenticated")
         assert found
-
-        #add a task assigned to 'fred'
         task1 = Task(title='Fleem', text='x', owner='fred', task_listID=tl.id)
-
-        #show the task
         app = self.getApp('auth')
 
-        res = app.get(url_for(
-                controller='task', action='show', id=task1.id))
-
+        ### make sure the task shows up
+        res = app.get(url_for(controller='task', action='show', id=task1.id))
         res.mustcontain('Fleem')
-        
-        #fill in the form
+
+        ### the comment should be returned in the response after it is submitted
         form = res.forms['add_comment_form']
-
         form.fields['text'][0].value = "This is a test comment."
-
         res = form.submit()
+        res.mustcontain("This is a test comment.")
 
+        ### and it should show up on the task's detail page, too
+        res = app.get(url_for(controller='task', action='show', id=task1.id))
         res.mustcontain("This is a test comment.")
 
         task1.destroySelf()
@@ -150,85 +134,123 @@ class TestTaskController(TestController):
         tl = self.create_tasklist('testing the auth role')
         #hack to force private tasks
         TaskListFeature(task_listID=tl.id, name='private_tasks', value=None)
-
         nonpriv = Task(title='The non-private one', text='x', private=False, task_listID=tl.id)
         priv = Task(title='The private one', text='x', private=True, task_listID=tl.id)
 
-        #admins can see everything
+        ### admins can see everything
         app = self.getApp('admin')
         res = app.get(url_for(controller='tasklist', action='show', id=tl.id))
         res.mustcontain('The non-private one')
         res.mustcontain('The private one')
 
-        #including next/prev navigation to the private task
+        ### including next/prev navigation to the private task
         res = app.get(url_for(controller='task', action='show', id=nonpriv.id))
         res.mustcontain("The private one")
         res = res.click("The private one")
         res.mustcontain("The private one")
 
-        #but mere members can't see private tasks
+        ### but mere members can't see private tasks
         app = self.getApp('member')
         res = app.get(url_for(controller='tasklist', action='show', id=tl.id))
         res.mustcontain('The non-private one')
         assert 'The private one' not in res.body
 
-        #not even in next/prev navigation on task/show
+        ### not even in next/prev navigation on task/show
         res = app.get(url_for(controller='task', action='show', id=nonpriv.id))
         assert 'The private one' not in res.body
 
-        #they also can't guess urls
+        ### they also can't guess urls
         res = app.get(url_for(controller='task', action='show', id=priv.id))
         location = res.header_dict['location']
         assert location.startswith('/error')
 
-        #but task owners can see their private tasks
+        ### but task owners can see their private tasks
         app = self.getApp('member')
         priv.owner = "member"
         res = app.get(url_for(controller='tasklist', action='show', id=tl.id))
         res.mustcontain('The non-private one')
         res.mustcontain('The private one')
         
-        #and can access them too
+        ### and can access them too
         res = res.click("The private one")
         res.mustcontain("The private one")
         
-        #even through next/prev navigation on task/show
+        ### even through next/prev navigation on task/show
         res = app.get(url_for(controller='task', action='show', id=nonpriv.id))
         res.mustcontain("The private one")
         res = res.click("The private one")
         res.mustcontain("The private one")
         
-        #cleanup time!
         nonpriv.destroySelf()
         priv.destroySelf()
 
     def test_task_update_field(self):
         tl = self.create_tasklist('testing task update', member_level=1, other_level=1)
-        task = Task(title='The task', text='x', private=False, task_listID=tl.id)
-        
+        task = Task(title='The task', text='x', private=False, task_listID=tl.id)        
         app = self.getApp('admin')
-        res = app.post(url_for(controller='task', action='assign', id=task.id, owner='newowner'))
-        res = app.get(url_for(controller='task', action='show', id=task.id))
         
-        res.mustcontain("newowner")
+        res = app.get(url_for(controller='tasklist', action='show', id=tl.id))
 
-        #find the old version
+        ### when you change a task owner, the response should consist of the new task TR
+        res = app.post('/task/change_field/%s' % task.id,
+                       params=dict(owner='Mowbray', field='owner', authenticator=self._get_authenticator(res)))
+        assert re.search('<tr[^>]*owner\s*=\s*"Mowbray"', res.body)
+
+        ### and the new owner should be displayed on the task page
+        res = app.get(url_for(controller='task', action='show', id=task.id))
+        res.mustcontain("Mowbray")
+
+        ### there should be a newly-creted version now
         versions = list(task.versions)
         assert len(versions) == 1
         version = versions[0]
 
+        ### and that version should reflect the original task information before the update
         assert version.owner == "" 
         assert version.dateArchived == task.created
 
-        version.destroySelf()
+        ### changing the status, deadline, and priority should do the same thing
+        res = app.post('/task/change_field/%s' % task.id,
+                       params=dict(status='true', field='status', authenticator=self._get_authenticator(res)))
+        assert re.search('<tr[^>]*status\s*=\s*"done"', res.body)
+        res = app.get(url_for(controller='task', action='show', id=task.id))
+
+        res = app.post('/task/change_field/%s' % task.id,
+                       params=dict(deadline='11-26-1984', field='deadline', authenticator=self._get_authenticator(res)))
+        assert re.search('<tr[^>]*deadline\s*=\s*"1984-11-26"', res.body)
+        res = app.get(url_for(controller='task', action='show', id=task.id))
+
+        res = app.post('/task/change_field/%s' % task.id,
+                       params=dict(priority='High', field='priority', authenticator=self._get_authenticator(res)))
+        assert re.search('<tr[^>]*priority\s*=\s*"High"', res.body)
+        res = app.get(url_for(controller='task', action='show', id=task.id))
+
+        ### and each of those actions should have created a new version
+        versions = list(task.versions)
+        assert len(versions) == 4
+
+        for version in versions:
+            version.destroySelf()
         task.destroySelf()
 
-        # now make sure that a member cannot do this
+        ### now make sure that a member cannot do this
         app = self.getApp('member')
         task = Task(title='The task', text='x', private=False, task_listID=tl.id)
-        res = app.post(url_for(controller='task', action='assign', id=task.id, owner='newowner'))
+
+        ### when he tries to change a field, the controller should throw us an AssertionError
+        res = app.get(url_for(controller='task', action='show', id=task.id))
+        try:
+            res = app.post('/task/change_field/%s' % task.id,
+                           params=dict(owner='member', field='owner', authenticator=self._get_authenticator(res)))
+            raise Exception("This should have failed.")
+        except AssertionError:
+            pass
+
+        ### his change should not be reflected in the task detail page
         res = app.get(url_for(controller='task', action='show', id=task.id))
         assert 'newowner' not in res
+
+        ### and no new version should be created
         versions = list(task.versions)
         assert len(versions) == 0
 
@@ -238,39 +260,20 @@ class TestTaskController(TestController):
     def can_claim_tasks(self):
         pass
 
-#    def test_task_update(self):
-#        tl = self.create_tasklist('testing task update')
+
+#    def _getElementsByTagName(self, body, tagname):
+#        """ This is a total hack and should not be used """
+#        elements = []
+#        start = -1
+#        while 1:
+#            start = body.find('<' + tagname, start + 1)
+#            if start == -1:
+#                break
+#            end = body.find('>', start)
+#            elements.append(body[start:end+1])
 #
-#        task = Task(title='The task', text='x', private=False, task_listID=tl.id)
-#        
-#        app = self.getApp('admin')
-#
-#        res = app.get(url_for(
-#                controller='task', action='show_update', id=task.id))
-#
-#        form = res.forms[0]
-#        form.fields['title'][0].value = "The updated task."
-#
-#        res = form.submit()
-#
-#        res = app.get(url_for(
-#                controller='task', action='show', id=task.id))
-#        
-#        res.mustcontain("The updated task")
-#
-#        #find the old version
-#        versions = task.versions
-#        assert len(versions) == 1
-#        version = versions[0]
-#
-#        assert version.title == "The task"
-#        assert version.text == "x"
-#        assert version.updated == task.created
-#
-#        version.destroySelf()
-#        task.destroySelf()
-#        tl.destroySelf()
-        
+#        return elements
+
 #     def test_task_watch(self):
 #         """Tests adding self as a watcher for a task"""
 #         tl = self.create_tasklist('testing task watching')
@@ -298,59 +301,3 @@ class TestTaskController(TestController):
 #         res = res.click("Stop watching")
 #         assert Watcher.selectBy(username='admin').count() == 0
 # actually, this role is never used.
-
-#     def test_auth_role(self):
-#         tl = self.create_tasklist('testing the auth role')
-
-#         #set security such that only task owner can change status and update
-#         found = False
-#         for perm in tl.permissions:
-#             if perm.action.action == "task_change_status" or perm.action.action == "task_update":
-#                 found = True
-#                 perm.min_level = Role.getLevel("TaskOwner")
-#         assert found
-
-#         #add a task assigned to 'auth'
-#         task1 = Task(title='Fleem', text='x', owner='auth', task_listID=tl.id)
-#         #add a task not assigned to 'auth'
-#         task2 = Task(title='Fleem', text='x', owner='admin', task_listID=tl.id)
-
-#         app = self.getApp('auth')
-
-#         res = app.get(url_for(
-#                 controller='tasklist', action='show', id=tl.id))
-
-#         res.mustcontain('testing the auth role')
-
-#         spanTags = self._getElementsByTagName(res.body, 'span')
-
-#         found = 0
-#         for span in spanTags:
-#             for field in 'status deadline priority owner'.split():
-#                 if '%s-label_%d' % (field, task1.id) in span:
-#                 #the label for task1 must be clickable
-#                     assert 'onclick="viewChangeableField(%d, &quot;%s&quot;)"' % (task1.id, field) in span
-#                     found += 1
-#                 elif '%s-label_%d' % (field, task2.id) in span:
-#                 #the label for task2 must not be clickable
-#                     assert 'onclick="viewChangeableField(%d, \'%s\')"' % (task1.id, field) not in span
-#                     found += 1
-
-#         assert found == 8
-
-#         selectTags = self._getElementsByTagName(res.body, 'select')
-#         found = 0
-#         for select in selectTags:
-#             #there is no select for task2, because we can't edit it.
-#             assert 'status_%d' % task2.id not in select 
-#             assert 'priority_%d' % task2.id not in select
-
-#             #but there is one for task1
-#             if 'status_%d' % task1.id in select or 'priority_%d' % task1.id in select:
-#               found += 1
-
-#         assert found == 2
-
-#         task1.destroySelf()
-#         task2.destroySelf()
-#         tl.destroySelf()
