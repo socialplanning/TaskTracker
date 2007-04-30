c3cc94e1ca0cd0d3535d069c55dfd5f541d680ea
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2007-04-30 21:05:21 +0000

    fixed test
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/trunk@4791 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- tasktracker/tests/functional/test_tasklist.py
+++ tasktracker/tests/functional/test_tasklist.py
@@ -324,7 +324,7 @@ class TestTaskListController(TestController):
         
         ### and then can do things as normal
         res = app.get(url_for(controller='tasklist'))
-        res.mustcontain("A task list is a container for tasks.")
+        res.mustcontain("Glossary")
 
     def test_task_create(self):
         """Tests creating a new task"""
