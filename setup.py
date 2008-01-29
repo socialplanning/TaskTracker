1bf93bddd80ab604b4106ae5ec4d74eef3ffc2f3
Rob Marianski <rob@marianski.com>
2008-01-29 17:38:58 +0000

    cabochon is actually required
    git-svn-id: https://svn.socialplanning.org/svn/TaskTracker/branches/0.1@13002 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
       "uuid",
       "topp.utils>0.1",
       "FormEncode==dev,>=0.7.2dev-r2661",
+      "CabochonClient",
+      "CabochonServer",
       "restclient",
       ],
     packages=find_packages(),
